4d5c8480db86f1cd3d7c5e6256797743ed275615
James Stack <james.a.stack@gmail.com>
2017-03-01 14:26:42 -0800

    Disable button functionality while toggling

--- skeleton/app/assets/javascripts/bundle.js
+++ skeleton/app/assets/javascripts/bundle.js
@@ -100,16 +100,28 @@ class FollowToggle {
   render() {
     if (this.followState === "false") {
       this.$el.html("Follow!");
+      this.$el.attr("disabled", false);
     } else if (this.followState === "true") {
       this.$el.html("Unfollow!");
+      this.$el.attr("disabled", false);
+    } else if (this.followState === "following") {
+      this.$el.attr("disabled", true);
+      this.followState = "false";
+    } else if (this.followState === "unfollowing") {
+      this.$el.attr("disabled", true);
+      this.followState = "true";
     }
   }
 
   handleClick(e) {
     e.preventDefault();
     if (this.followState === "false"){
+      this.followState = "following";
+      this.render();
       APIUtil.followUser(this.userId).then(() => this.toggleFollow());
     } else {
+      this.followState = "unfollowing";
+      this.render();
       APIUtil.unfollowUser(this.userId).then(() => this.toggleFollow());
     }
 
